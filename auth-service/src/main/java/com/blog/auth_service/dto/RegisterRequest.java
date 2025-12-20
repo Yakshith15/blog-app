d16feaf6d52f26663e1a55028584472418b0c28b
Yakshith15 <daggupatiyakshithnaidu@gmail.com>
2025-12-20 11:26:24 +0530

    input validations in auth svc

--- auth-service/src/main/java/com/blog/auth_service/dto/RegisterRequest.java
+++ auth-service/src/main/java/com/blog/auth_service/dto/RegisterRequest.java
@@ -1,9 +1,21 @@
 package com.blog.auth_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class RegisterRequest {
 
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be 3–20 characters")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
     public String getEmail() {
