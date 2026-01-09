b6584fcee1b1158f377071e41850c1014f78e7e1
Yakshith15 <daggupatiyakshithnaidu@gmail.com>
2026-01-09 09:02:24 +0530

    s3 client:

--- media-service/src/server.ts
+++ media-service/src/server.ts
@@ -3,6 +3,8 @@ import { env } from "./config/env";
 
 const port = env.PORT;
 
+
+console.log("AWS REGION:", env.AWS_REGION);
 app.listen(port, () => {
   console.log(`Media service running on port ${port}`);
 });
